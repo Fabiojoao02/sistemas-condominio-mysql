71504257eec28334ab3eb52d1bdb1f084d4a5de4
FabioJoao <fabiojoaoanastacio@hotmail.com>
2023-04-01 20:38:58 -0300

    Introdução ao relatori DPF

--- movimentacao/urls.py
+++ movimentacao/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 # from django.views.generic.list import ListView
-from .views import ListaLeitura, ListaCalculo
+from .views import ListaLeitura, ListaCalculo, RelatorioCalculos
 
 app_name = 'movimentacao'
 
 urlpatterns = [
     path('', ListaCalculo.as_view(), name='listacalculo'),
     path('listaleitura/', ListaLeitura.as_view(), name='listaleitura'),
+    path('relatoriocalculos/', RelatorioCalculos.as_view(),
+         name='relatoriocalculos'),
+
 ]
--- movimentacao/views.py
+++ movimentacao/views.py
@@ -7,7 +7,70 @@ from django.contrib import messages
 # from django.views import View
 from . models import Leituras, Calculos
 from django.db import connection
-# from django.db.models import Count
+# from django.db.models import
+import io
+from django.http import FileResponse
+from django.views.generic import View
+
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+
+class RelatorioCalculos(View):
+
+    def get(sel, request, *args, **kwargs):
+        # cria um arquivo para receber os dados e gerar o PDF
+        buffer = io.BytesIO()
+
+        # cria o arquivo PDF
+        pdf = canvas.Canvas(buffer, pagesize=letter, bottomup=0)
+        # cria o texto de objetos
+        textob = pdf.beginText()
+        textob.setTextOrigin(inch, inch)
+        textob.setFont("Helvetica", 14)
+
+        # leturas dos dados na tabela da query
+        relatorios = Calculos.objects.raw("\
+                select id_calculos, \
+                concat(left(cal.mesano,2),'/',right(cal.mesano,4)) as mesano,\
+                m.apto_sala, cad.nome morador, \
+                c.nome conta,\
+                ROUND(valor,2) valor\
+                from calculos cal\
+                join contas c on\
+                c.id_conta = cal.id_contas\
+                join morador m on\
+                m.id_morador = cal.id_morador\
+                join cadastro cad on\
+                cad.id_cadastro = case when responsavel='I' then id_inquilino else id_proprietario end\
+                where cal.mesano = 022023 and cal.id_morador = 1\
+         ")
+       # relatorios = Calculos.objects.all()
+        # lines = []
+
+        # for relatorio in relatorios:
+        #     lines.append(str(relatorio.id_morador))
+        #     lines.append(str(relatorio.id_contas))
+        #     lines.append(str(relatorio.valor))
+        #     lines.append(str(relatorio.mesano))
+        #     lines.append(" ")
+
+        # inserir dados no PRDF LOOP
+        for relatorio in relatorios:
+            # print(relatorio)
+            textob.textLine(str(relatorio.mesano))
+            textob.textLine(str(relatorio.apto_sala))
+        pdf.drawText(textob)
+
+        # quando acabamos de inserir no dpf
+        pdf.showPage()
+        pdf.save()
+
+        # fim, retorna o buffer para o inicio do arquivo
+        buffer.seek(0)
+
+        return FileResponse(buffer, as_attachment=True, filename='relcal.pdf')
 
 
 class ListaLeitura(TemplateView):
